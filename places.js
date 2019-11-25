37a21f2486d936b399bceb0be1ba5c30a7af9447
Gaston Pisacco <gaston@droptek.com>
2019-11-24 20:33:37 -0800

    Unit Test added

--- places.js
+++ places.js
@@ -6,8 +6,7 @@ exports.handler = async (event, context) => {
     const HERE_APP_ID = process.env.HereAppId;
     const HERA_APP_CODE = process.env.HereAppCode;
     const HERE_BASE_URL = process.env.HereBaseURL;
-   
-
+    
     const url  = `${HERE_BASE_URL}/discover/explore?at=52.5159%2C13.3777&cat=accommodation&app_id=${HERE_APP_ID}&app_code=${HERA_APP_CODE}`;
 
     const response = await fetch(url);
